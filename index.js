2fb9df1629ba5a95af8c651fb9df97ea5a1c3fd4
fmaieli <maieli.franco@gmail.com>
2020-11-03 15:01:11 -0300

    Removed not necessary html elements

--- index.js
+++ index.js
@@ -51,16 +51,6 @@ $(document).ready(() => {
         $("#status").text("Activate: " + data.node);
       }
     });
-
-    // Activate a node, on button click
-    $("#button1").click(function() {
-      var tree = $("#tree").fancytree("getTree"),
-        node2 = tree.getNodeByKey("id2");
-      node2.toggleSelected();
-    });
-    $("#version").text(
-      "Fancytree " + $.ui.fancytree.version + " / jQuery " + $.fn.jquery
-    );
   });
 
   //----------------------------------------------------------------------
