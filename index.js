83b62af32d5450ee9270d3bc450cf5a901ce7f58
fmaieli <maieli.franco@gmail.com>
2021-05-13 12:26:42 -0300

    deleted an import of styles.css, repetion

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
 // Imports
-var $ = require("jquery");
-var jQuery = require("jquery");
-import "jquery.fancytree";
-import AjaxCalls from "~/ajax-calls/ajax-calls.service.js";
-import DropdownTreeWrapper from "~/wrappers/dropdownTreeWrapper.js";
-import "./style.css";
-import FancyTreeWrapper from "~/wrappers/fancyTreeWrapper.js";
-import TreeComponent from "./treeComponent.js";
-import "./style.css";
+var $ = require('jquery');
+var jQuery = require('jquery');
+import 'jquery.fancytree';
+import AjaxCalls from '~/ajax-calls/ajax-calls.service.js';
+import DropdownTreeWrapper from '~/wrappers/dropdownTreeWrapper.js';
+import './style.css';
+import FancyTreeWrapper from '~/wrappers/fancyTreeWrapper.js';
+import TreeComponent from './treeComponent.js';
 
 // Json Data
-import jsonData from "./jsonDataDummy/tiposDeEnsayo.js";
+import jsonData from './jsonDataDummy/tiposDeEnsayo.js';
 
 // Ajax calls
 var ajaxCalls = new AjaxCalls();
 
 // Wrappers
 var dropdownTreeWrapper = new DropdownTreeWrapper(
-  "-seleccione-",
+  '-seleccione-',
   300,
   true,
   true,
-  "dropdown-tree"
+  'dropdown-tree'
 );
-var fancyTreeWrapper = new FancyTreeWrapper("fancytree");
+var fancyTreeWrapper = new FancyTreeWrapper('fancytree');
 
 // Tree Component
 var treeComponentDropdown = new TreeComponent(
   dropdownTreeWrapper,
-  "/GestionEnsayosApi/ListTiposDeEnsayoExistentes",
+  '/GestionEnsayosApi/ListTiposDeEnsayoExistentes',
   ajaxCalls,
   jsonData
 );
@@ -36,53 +35,53 @@ treeComponentDropdown.initializeComponent();
 
 var treeComponentFancy = new TreeComponent(
   fancyTreeWrapper,
-  "/ComparativoVariedades/CargarFancyTree",
+  '/ComparativoVariedades/CargarFancyTree',
   ajaxCalls,
   jsonData
 );
 treeComponentFancy.initializeComponent();
 
 $(document).ready(() => {
-  const fancytreeDiv = document.getElementById("fancytree-title");
+  const fancytreeDiv = document.getElementById('fancytree-title');
   fancytreeDiv.innerHTML = `<h1>Fancy Tree</h1>`;
 
-  const dropdowntreeDiv = document.getElementById("dropdown-tree-title");
+  const dropdowntreeDiv = document.getElementById('dropdown-tree-title');
   dropdowntreeDiv.innerHTML = `<h1>Dropdown Tree</h1>`;
 
   // ------------------------------Button---------------------------------
-  $("#selectedButton").on("click", function() {
+  $('#selectedButton').on('click', function() {
     var itemsDropdown = treeComponentDropdown.selectedItems();
     var itemsFancy = treeComponentFancy.selectedItems();
   });
 
   //Inicio - Dejar submenú abierto hasta que se hace click fuera del menú
-  jQuery(".dropdown-menu li a").mouseover(function(e) {
+  jQuery('.dropdown-menu li a').mouseover(function(e) {
     e.stopPropagation();
     jQuery(this)
       .parent()
       .parent()
-      .find("li")
+      .find('li')
       .each(function() {
-        jQuery(this).removeClass("open");
+        jQuery(this).removeClass('open');
       });
     jQuery(this)
       .parent()
-      .addClass("open");
+      .addClass('open');
   });
 
-  jQuery(".dropdown-toggle").click(function(e) {
+  jQuery('.dropdown-toggle').click(function(e) {
     var classList = jQuery(this)
       .parent()
-      .attr("class")
+      .attr('class')
       .split(/\s+/);
-    if (classList.includes("open")) {
+    if (classList.includes('open')) {
       jQuery(this)
         .parent()
-        .removeClass("open");
+        .removeClass('open');
     } else {
       jQuery(this)
         .parent()
-        .addClass("open");
+        .addClass('open');
     }
   });
   //Fin - Dejar submenú abierto hasta que se hace click fuera del menú
